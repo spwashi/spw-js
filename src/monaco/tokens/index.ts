fb011497b46805a64478324de107f90abf80ca81
Sam Washington <sam@spwashi.com>
2021-06-24 22:17:10 -0500

    changed the naming scheme of monaco monarch tokens to be a bit less volatile

--- src/monaco/tokens/index.ts
+++ src/monaco/tokens/index.ts
@@ -36,9 +36,9 @@ import { ContainerNode } from '@constructs/ast/nodes/containers/_abstract/contai
 
 export const tok_whitespace = 'whitespace';
 
-export const rn_node = Node.name;
+export const rn_node = Node.kind;
 export const rn_anchorNode = 'node_anchor';
-export const rn_container = ContainerNode.name;
+export const rn_container = ContainerNode.kind;
 
 export const tok_anchorNode = 'anchor';
 
@@ -55,57 +55,57 @@ export const tok_stringQuote = rn_stringNode_open + '.quote';
 const suffix_body = '__body';
 
 // domain
-export const rn_domain_open = DomainSchemeOperator.name;
-export const rn_domain_body = Domain.name + suffix_body;
-export const rn_domain_close = DomainIdentityOperator.name;
+export const rn_domain_open = DomainSchemeOperator.kind;
+export const rn_domain_body = Domain.kind + suffix_body;
+export const rn_domain_close = DomainIdentityOperator.kind;
 
-export const tok_domain = rn_container + '.' + Domain.name;
+export const tok_domain = rn_container + '.' + Domain.kind;
 
 // essence
 
-export const rn_essence_open = EssentialSchemeOperator.name;
-export const rn_essence_body = EssentialSchemeOperator.name + suffix_body;
-export const rn_essence_close = EssentialIdentityOperator.name;
+export const rn_essence_open = EssentialSchemeOperator.kind;
+export const rn_essence_body = EssentialSchemeOperator.kind + suffix_body;
+export const rn_essence_close = EssentialIdentityOperator.kind;
 
-export const tok_essence = rn_container + '.' + Essence.name;
+export const tok_essence = rn_container + '.' + Essence.kind;
 
 // location
 
-export const rn_location_open = LocationalSchemeOperator.name;
-export const rn_location_body = Location.name + suffix_body;
-export const rn_location_close = LocationalIdentityOperator.name;
+export const rn_location_open = LocationalSchemeOperator.kind;
+export const rn_location_body = Location.kind + suffix_body;
+export const rn_location_close = LocationalIdentityOperator.kind;
 
-export const tok_location = rn_container + '.' + Location.name;
+export const tok_location = rn_container + '.' + Location.kind;
 
 // concept
 
-export const rn_concept_open = ConceptSchemeOperator.name;
-export const rn_concept_body = Concept.name + suffix_body;
-export const rn_concept_close = ConceptualIdentityOperator.name;
+export const rn_concept_open = ConceptSchemeOperator.kind;
+export const rn_concept_body = Concept.kind + suffix_body;
+export const rn_concept_close = ConceptualIdentityOperator.kind;
 
-export const tok_concept = rn_container + '.' + Concept.name;
+export const tok_concept = rn_container + '.' + Concept.kind;
 
 // operators
 
 export const rn_operator = 'operator';
 
-export const tok_aggregation = rn_operator + '.' + AggregationOperator.name;
-export const tok_ascent = rn_operator + '.' + AscentOperator.name;
-export const tok_branch = rn_operator + '.' + BranchOperator.name;
-export const tok_channel = rn_operator + '.' + ChannelOperator.name;
-export const tok_descent = rn_operator + '.' + DescentOperator.name;
-export const tok_direction = rn_operator + '.' + DirectionOperator.name;
-export const tok_evaluation = rn_operator + '.' + EvaluationOperator.name;
-export const tok_invocation = rn_operator + '.' + InvocationOperator.name;
-export const tok_performance = rn_operator + '.' + PerformanceOperator.name;
-export const tok_perspective = rn_operator + '.' + PerspectiveOperator.name;
-export const tok_range = rn_operator + '.' + RangeOperator.name;
-export const tok_reduction = rn_operator + '.' + ReductionOperator.name;
-export const tok_reference = rn_operator + '.' + ReferenceOperator.name;
-export const tok_spread = rn_operator + '.' + SpreadOperator.name;
-export const tok_transformation = rn_operator + '.' + TransformationOperator.name;
-export const tok_value = rn_operator + '.' + ValueOperator.name;
+export const tok_aggregation = rn_operator + '.' + AggregationOperator.kind;
+export const tok_ascent = rn_operator + '.' + AscentOperator.kind;
+export const tok_branch = rn_operator + '.' + BranchOperator.kind;
+export const tok_channel = rn_operator + '.' + ChannelOperator.kind;
+export const tok_descent = rn_operator + '.' + DescentOperator.kind;
+export const tok_direction = rn_operator + '.' + DirectionOperator.kind;
+export const tok_evaluation = rn_operator + '.' + EvaluationOperator.kind;
+export const tok_invocation = rn_operator + '.' + InvocationOperator.kind;
+export const tok_performance = rn_operator + '.' + PerformanceOperator.kind;
+export const tok_perspective = rn_operator + '.' + PerspectiveOperator.kind;
+export const tok_range = rn_operator + '.' + RangeOperator.kind;
+export const tok_reduction = rn_operator + '.' + ReductionOperator.kind;
+export const tok_reference = rn_operator + '.' + ReferenceOperator.kind;
+export const tok_spread = rn_operator + '.' + SpreadOperator.kind;
+export const tok_transformation = rn_operator + '.' + TransformationOperator.kind;
+export const tok_value = rn_operator + '.' + ValueOperator.kind;
 
-export const tok_commonDelimiter = rn_operator + '.' + CommonDelimitingOperator.name;
-export const tok_blockDelimiter = rn_operator + '.' + BlockDelimitingOperator.name;
-export const tok_operatorDelimiter = rn_operator + '.' + OperatorDelimitingOperator.name;
+export const tok_commonDelimiter = rn_operator + '.' + CommonDelimitingOperator.kind;
+export const tok_blockDelimiter = rn_operator + '.' + BlockDelimitingOperator.kind;
+export const tok_operatorDelimiter = rn_operator + '.' + OperatorDelimitingOperator.kind;
